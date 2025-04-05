32a83263938f226e003cb0be890dba05b587af67
William D Leao <wdsbleao@gmail.com>
2025-04-05 19:51:00 -0300

    Primeira pratica

---
+++ simulation/qsim/work/mic1/_primary.vhd
@@ -0,0 +1,12 @@
+library verilog;
+use verilog.vl_types.all;
+entity mic1 is
+    port(
+        \00\            : out    vl_logic;
+        \10\            : in     vl_logic;
+        \11\            : in     vl_logic;
+        \01\            : out    vl_logic;
+        \02\            : out    vl_logic;
+        \03\            : out    vl_logic
+    );
+end mic1;
