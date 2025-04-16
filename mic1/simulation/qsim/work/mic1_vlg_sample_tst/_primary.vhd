59754e5ed0389c3ac47b394a4359a4729960c7c9
William D Leao <wdsbleao@gmail.com>
2025-04-16 19:56:48 -0300

    Reorganizing

--- mic1/simulation/qsim/work/mic1_vlg_sample_tst/_primary.vhd
+++
@@ -1,9 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity mic1_vlg_sample_tst is
-    port(
-        pin_name1       : in     vl_logic;
-        pin_name2       : in     vl_logic;
-        sampler_tx      : out    vl_logic
-    );
-end mic1_vlg_sample_tst;
